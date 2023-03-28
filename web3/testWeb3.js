26290f4bfd48c5908d4c0da676ddd0e959eec483
Jastagar <jastagar1581.be20@chitkara.edu.in>
2023-03-28 15:31:23 +0530

    pass hidden

--- web3/testWeb3.js
+++ web3/testWeb3.js
@@ -3,6 +3,6 @@ const {transferKCO} = require('./web3Wallet')
 const fromAddress = '0x2ee4961905E3c9B6eC890d5F919224Ad6BD87637'
 const toAddress = '0x879005ce3b64a880e1512d759cecb1bd857590f8'
 const amount = 100
-const pass = "Iamjastagar1@mks"
+// const pass = "" // your passs here
 
 transferKCO(fromAddress,toAddress,amount,pass)
