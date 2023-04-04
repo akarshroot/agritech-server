73332010679cb45c66de2f8dc94c915eaef2e71f
Jastagar <jastagar1581.be20@chitkara.edu.in>
2023-04-04 12:15:23 +0530

    funding working so far

--- web3/compile.js
+++ web3/compile.js
@@ -1,27 +1,39 @@
 const solc = require("solc")
-const fs = require("fs")
+const fs = require("fs");
+const {info}=require("../utils/logger");
 
-const fileAddress = __dirname+'/contracts/funding.sol'
+const fileAddress = __dirname+'/contracts/funding2.sol'
+const fileAddress2 = __dirname+'/contracts/IERC20.sol'
 const readFile = fs.readFileSync(fileAddress,'utf8');
+const readFile2 = fs.readFileSync(fileAddress2,'utf8');
+
+function findImports(path) {
+    info(path)
+      return {
+        contents:
+          readFile2
+      }
+  }
 
 var input = {
     language: 'Solidity',
     sources: {
-        'funding.sol': {
-        content: readFile.toString()
+        'funding2.sol': {
+            content: readFile.toString()
         }
     },
     settings: {
         outputSelection: {
-        '*': {
-            '*': ['*']
-        }
+            '*': {
+                '*': ['*']
+            }
         }
     }
 };
 
-const output = JSON.parse(solc.compile(JSON.stringify(input)));
-const outputContract = output.contracts['funding.sol'].kissanFundContract
+const output = JSON.parse(solc.compile(JSON.stringify(input), { import: findImports }));
+// info(output)
+const outputContract = output.contracts['funding2.sol'].kissanFundContract
 
 module.exports = {
     compiledAPi:      outputContract.abi,
--- web3/testWeb3.js
+++ web3/testWeb3.js
@@ -1,22 +1,19 @@
+const {info}=require('../utils/logger')
+const {FundingABI}=require('./contracts/ABIs')
+const web3=require('./web3')
 const {transferKCO, giveApproval} = require('./web3Wallet')
 
 const fromAddress = '0x879005ce3b64a880e1512d759cecb1bd857590f8'
-const toAddress = '0x1cE8c5Ccf95154C3B5A806f90392B62A1540052e'
+const toAddress = '0xEa290a8F4fFdf0ca97ccf721c16812F71f8Deffb'
 const amount = 5000
 const pass = "1234567890" // your passs here
 
-// transferKCO(fromAddress,toAddress,amount,pass)
 
-giveApproval(
-    fromAddress,
-    toAddress,
-    pass,
-    amount
-)
-
-// transferKCO(
-// 	'0x2ee4961905e3c9b6ec890d5f919224ad6bd87637',
-// 	'0x879005ce3b64a880e1512d759cecb1bd857590f8',
-// 	99,
-// 	'Iamjastagar1@mks'
-// 	)
+const contract = new web3.eth.Contract(FundingABI,'0x8C5456399494739738e97688Dcf15184b6585e16')
+web3.eth.personal.unlockAccount(fromAddress,pass,300).then(()=>{
+    contract.methods.contribute(100).send({
+        from:fromAddress
+    }).then((res) => {
+        info(res)
+    })
+})
