851adf23872493d74bd932d0f149c1999293580a
nerdy-monk <akarshtripathi03@gmail.com>
2023-04-06 23:39:14 +0530

    Fetch user campaigns endpoint

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,14 +2,15 @@ const { Router } = require("express")
 const User = require("../models/User.js")
 const auth = require("../middleware/auth.js")
 const { info, err } = require("../utils/logger.js");
+const Campaign = require("../models/Campaign.js");
 
 const router = Router();
 
 router.post("/data", auth, async (req, res) => {
     try {
         const id = req.body.userId
-        const user = await User.findOne({_id: id})
-        if(!user) res.status(400).json({error: true, message: "User not found."})
+        const user = await User.findOne({ _id: id })
+        if (!user) res.status(400).json({ error: true, message: "User not found." })
         else {
             user.password = undefined
             res.status(200).json({
@@ -19,8 +20,25 @@ router.post("/data", auth, async (req, res) => {
         }
     } catch (error) {
         err(error)
-        res.status(500).json({error: true, message: "Internal Server Error"})
+        res.status(500).json({ error: true, message: "Internal Server Error" })
     }
 })
 
+router.post("/campaigns", auth, async (req, res) => {
+    try {
+        const id = req.body.userId
+        const campaigns = await Campaign.find({ manager: id })
+        info(campaigns)
+        if (!campaigns) res.status(200).json({ error: true, message: "No campaigns found." })
+        else {
+            res.status(200).json({
+                error: false,
+                data: campaigns
+            })
+        }
+    } catch (error) {
+        err(error)
+        res.status(500).json({ error: true, message: "Internal Server Error" })
+    }
+})
 module.exports = router
