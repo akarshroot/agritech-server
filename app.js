7ac5ee2fc36f48f64176d7df77b373e8806884ef
Jastagar <jastagar1581.be20@chitkara.edu.in>
2023-04-11 20:36:55 +0530

    build ready

--- app.js
+++ app.js
@@ -16,35 +16,24 @@ const web3RouterVoting=require("./routes/web3RouterVoting");
 app.use(cookies());
 app.use(cors())
 app.use(express.json())
-
+app.use(express.static('build'));
 
 mongoose.connect(`mongodb+srv://admin:${process.env.MONGO_PASS}@maincluster.yajbyem.mongodb.net/?retryWrites=true&w=majority`)
 
 mongoose.connection.on('connected', function () {
     info('Mongoose connection open');
-    // new User({
-    //     name: "Test",
-    //     email: "test@test.com",
-    //     phno: 0,
-    //     password: "test",
-    // }).save()
 });
-
-app.get('/', (req, res) => {
-    info("Home")
-    res.send("AgriTech Server")
-})
-
-// setAddress().then(() => {
 app.use('/api/web3/wallet', web3Router)
 app.use('/api/web3/fundingContracts', web3RouterFunding)
 app.use('/api/web3/votingContracts', web3RouterVoting)
-// })
 app.use("/api/auth", authRoutes)
 app.use("/api/refreshToken", refreshTokenRoute)
 app.use("/api/user", userRoutes)
 app.use("/api/store", storeRoutes)
 app.use("/api/wallet/", walletRoutes)
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/build/index.html'));
+  });
 
 module.exports = app
